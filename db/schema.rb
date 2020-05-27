5071f704acbf1977942f0a34a817970ec284a880
shoichi.sugiyama <syouiti0722@gmail.com>
2020-05-27 12:32:19 +0000

    post

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_191210) do
-
-  create_table "makings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_makings_on_user_id"
-  end
-
-  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_microposts_on_user_id"
-  end
-
-  create_table "producings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_producings_on_user_id"
-  end
-
-  create_table "task_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_task_lists_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_05_27_122709) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status", limit: 10
+    t.string "status"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -59,8 +29,5 @@ ActiveRecord::Schema.define(version: 2020_05_25_191210) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "makings", "users"
-  add_foreign_key "microposts", "users"
-  add_foreign_key "producings", "users"
-  add_foreign_key "task_lists", "users"
+  add_foreign_key "tasks", "users"
 end
