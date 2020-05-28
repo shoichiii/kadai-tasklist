37c579d6c6601c212c8f7efd092d3c90b4a87d82
shoichi.sugiyama <syouiti0722@gmail.com>
2020-05-28 12:58:33 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :show, :edit]
-  before_action :correct_user, only: [:show]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit]
   def index
-    if logged_in?
       @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
   end
 
   def show
