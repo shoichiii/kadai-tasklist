5f245e42d855a8bb84618512e751c29920e7c7c9
shoichi.sugiyama <syouiti0722@gmail.com>
2020-05-29 06:03:36 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :edit, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   def index
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
@@ -46,9 +46,6 @@ class TasksController < ApplicationController
   
   private
 
-  def set_task
-    @task = Task.find(params[:id])
-  end
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
